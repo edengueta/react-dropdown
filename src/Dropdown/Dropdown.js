dc86b8e6cc2c65e7c90157a2c784e5ec6a2fa7ff
“Your <you@example.com>
2021-01-31 13:14:57 +0200

    Outside click fixed

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,23 +1,24 @@
 import React, { useRef, useEffect } from 'react';
-import './Dropdown.css'
 
 function Dropdown(props) {
 
     const ref = useRef();
 
     useEffect(() => {
+
         document.addEventListener("click", HandleOutsideClick);
-    },[]);
+        return () => document.removeEventListener("click", HandleOutsideClick);
+
+    }, [HandleOutsideClick] );
 
     function HandleOutsideClick(e) {
         if (ref.current && !ref.current.contains(e.target)) {
             if (props.isShown) props.onOutsideClick();
-            console.log(props.onOutsideClick);
         }
     };
     
     return (
-        <nav ref={ref} className="Dropdown">
+        <nav ref={ref}>
             {props.children}
         </nav>
     );
--- src/Dropdown/DropdownMenu/DropdownMenu.js
+++ src/Dropdown/DropdownMenu/DropdownMenu.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import './DropdownMenu.css';
 
 function DropdownMenu(props) {
